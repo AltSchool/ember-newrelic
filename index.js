673b054d240328b1cd2e7b7bd5ab7ea71f8edd52
Alex Gibbons <alex.gibbons@gmail.com>
2015-09-02 20:42:03 -0700

    Add configuration flag to enable addon

--- index.js
+++ index.js
@@ -25,13 +25,16 @@ module.exports = {
 
   contentFor: function(type, config) {
     var content = '';
+    var enabled = config.newrelicEnabled || config.environment === 'production';
 
-    if (config.environment !== 'test' && type === 'head') {
-      var content = readSnippet();
+    // Warn when no snippet found in development regardless of whether new relic
+    // is enabled.
+    if (config.environment === 'development' && !readSnippet()) {
+      console.warn('No New Relic snippet found, run `ember generate newrelic-license <app-name> <api-key>`');
+    }
 
-      if (!content) {
-        console.warn('New Relic disabled: no snippet found, run `ember generate newrelic-license <app-name> <api-key>`');
-      }
+    if (enabled && type === 'head') {
+      content = readSnippet();
     }
 
     return content;
