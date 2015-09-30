4a76f41f8b0ccc8d32835caa6f3a6f70ca933dfa
Alex Gibbons <alex.gibbons@gmail.com>
2015-09-30 11:01:53 -0700

    Use New Relic loader endpoint to fetch app independent snippet

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ember-newrelic
 
-Adds `NewRelic` tracking to your Ember app.
+Adds `New Relic` tracking to your Ember app.
 
 ## Install
 
@@ -8,11 +8,21 @@ Adds `NewRelic` tracking to your Ember app.
 ember install git+ssh://git@github.com:AltSchool/ember-newrelic.git
 ```
 
-## Generate New Relic Snippet
+During the install process the latest New Relic snippet will be placed into `vendor/newrelic-snippet.js`.
 
-You'll need to obtain an [API key](https://docs.newrelic.com/docs/apis/rest-api-v2/requirements/rest-api-key#viewing) from New Relic in order to run this command. Alternately you can copy and paste 
-the contents of the snippet into `vendor/newrelic-snippet.html`.
+## Configure
+
+To enable New Relic integration add the following configuration to `app/config/environment.js`
 
 ```
-ember generate newrelic-license <app-name> <api-key>
+ENV.newRelic = {
+  appId: "MY_NEW_RELIC_APP_ID",
+  licenseKey: "MY_NEW_RELIC_LICENSE_KEY"
+};
+```
+
+If you need to update or regenerate your snippet run:
+
+```
+ember generate ember-newrelic
 ```
--- index.js
+++ index.js
@@ -5,9 +5,16 @@ var fs = require('fs');
 var path = require('path');
 
 
+function getSnippet(appId, licenseKey) {
+  return wrapSnippetInScriptTags([
+    readSnippet(),
+    generateSnippetConfig(appId, licenseKey)
+  ].join('\n'));
+}
+
 function readSnippet() {
   try {
-    return fs.readFileSync(path.join(process.cwd(), 'vendor/newrelic-snippet.html'), {
+    return fs.readFileSync(path.join(process.cwd(), 'vendor/newrelic-snippet.js'), {
       encoding: 'UTF-8'
     });
   } catch(error) {
@@ -19,22 +26,36 @@ function readSnippet() {
   }
 }
 
+function generateSnippetConfig(appId, licenseKey) {
+  return [
+    'NREUM.info = {',
+      'applicationID: "'+ appId +'",',
+      'licenseKey:    "'+ licenseKey +'"',
+    '};'
+  ].join('\n');
+}
+
+function wrapSnippetInScriptTags(snippet) {
+  return [
+    '<script type="text/javascript">',
+      snippet,
+    '</script>'
+  ].join('\n');
+}
+
 
 module.exports = {
   name: 'ember-newrelic',
 
   contentFor: function(type, config) {
     var content = '';
-    var enabled = config.newrelicEnabled || config.environment === 'production';
-
-    // Warn when no snippet found in development regardless of whether new relic
-    // is enabled.
-    if (config.environment === 'development' && !readSnippet()) {
-      console.warn('No New Relic snippet found, run `ember generate newrelic-license <app-name> <api-key>`');
-    }
+    var newRelicConfig = config.newRelic || {};
+    var appId = newRelicConfig.appId;
+    var licenseKey = newRelicConfig.licenseKey;
+    var enabled = appId && licenseKey;
 
     if (enabled && type === 'head') {
-      content = readSnippet();
+      content = getSnippet(appId, licenseKey);
     }
 
     return content;
